9f903415a7ea5c7c4ffd1dcabaaa28e1c5dbaca2
Indrajeet Singh <indrajeetsingh0796@gmail.com>
2018-10-01 12:48:35 +0530

    with testimonial

--- footer.php
+++ footer.php
@@ -48,7 +48,5 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <!-- Bootstrap JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <!-- My js files -->
-        <script src="./script.js"></script>
     </body>
 </html>
